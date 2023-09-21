bdffdb8086dd576e91d014c6016cee9238a46ad4
Dessislavaa <145642759+Dessislavaa@users.noreply.github.com>
2023-09-21 08:46:33 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protokol12
+{
+   public class Person
+    {
+         public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string Profession { get; set; }
+        public string sex { get; set; }
+    }
+}
